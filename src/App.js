41b89ce3cebf05f050b4d6d0fc9ffd5080bf4c93
sattarzhanovdev <sattarzhanovdev@gmail.com>
2025-08-09 14:14:55 +0600

    push

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import './App.css'
 import { BrowserRouter as Router } from 'react-router-dom';
 
-axios.defaults.baseURL = 'https://canadacrm.pythonanywhere.com/api'; // Set your API base URL here
+axios.defaults.baseURL = 'https://maxthebest.pythonanywhere.com/api'; // Set your API base URL here
 
 const App = () => (
   <Router>
